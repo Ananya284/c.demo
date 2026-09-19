165a456ac5c4a0fe0006dfbd600a9b2903cf21c9
student Ananya <dasananya0120@gmail.com>
2026-09-19 09:03:12 +0530

    modified

--- c/C/if_statement.c
+++ c/C/if_statement.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int age;
-    printf("enter your age");
+    printf("enter your age:");
     scanf("%d",&age);
     if(age>=18){
         printf("eligible to vote");

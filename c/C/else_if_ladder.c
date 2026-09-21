36cd1f15cdc2512a13ba6851e7597b0f5ca42af3
student Ananya <dasananya0120@gmail.com>
2026-09-22 01:25:36 +0530

    add this file

---
+++ c/C/else_if_ladder.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d,e;
+    printf("enter 5 numbers");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    if(a>=b&&a>=c&&a>=d&&a>=e){
+        printf("%d is maximum",a);
+    }
+    else if(b>=a&&b>=c&&b>=d&&b>=e){
+        printf("%d is maximum",b);
+    }
+    else if(c>=a&&c>=b&&c>=d&&c>=e){
+        printf("%d is maximum",c);
+    }
+    else if(d>=a&&d>=b&&d>=c&&d>=e){
+        printf("%d is maximum",d);
+    }
+    else{
+        printf("%d is maximum",e);
+    }
+    return 0;
+    
+
+}
